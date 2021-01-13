f195ddf03187506cf6680f6dd9a3d7af95550e46
ArcherWoo <sicong.wu@hotmail.com>
2021-01-13 23:26:28 +0800

    Update binarysearch

--- BinarySearch/BinarySearch.c
+++ BinarySearch/BinarySearch.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int BinarySearch(int *arr, int len, int data)
 {
@@ -8,8 +9,7 @@ int BinarySearch(int *arr, int len, int data)
 
 	while(min <= max)
 	{
-		mid = min + ((max - min)>>1);
-		// if (arr[mid] < data) 从大到小
+		mid = min + (max - min)>>1;
 		if (arr[mid] > data)
 		{
 			max = mid - 1;
@@ -26,11 +26,72 @@ int BinarySearch(int *arr, int len, int data)
 	return -1;
 }
 
+int left_bound(int *arr, int len, int data)
+{
+	int min = 0;
+	int max = len - 1;
+	int mid;
+
+	while(min <= max)
+	{
+		mid = min + (max - min) / 2;
+		if (arr[mid] > data)
+		{
+			max = mid - 1;
+		}
+		else if (arr[mid] < data)
+		{
+			min = mid + 1;
+		}
+		else if (arr[mid] == data)
+		{
+			max = mid - 1;
+		}
+	}
+
+	if (min >= len || arr[min] != data)
+	{
+		return -1;
+	}
+	return min;
+}
+
+int right_bound(int *arr, int len, int data)
+{
+	int min = 0;
+	int max = len - 1;
+	int mid;
+
+	while(min <= max)
+	{
+		mid = min + (max - min) / 2;
+		if (arr[mid] > data)
+		{
+			max = mid - 1;
+		}
+		else if (arr[mid] < data)
+		{
+			min = mid + 1;
+		}
+		else
+		{
+			min = mid + 1;
+		}
+	}
+
+	if (max < 0 || arr[max] != data)
+	{
+		return -1;
+	}
+	return max;
+}
+
 int main(int argc, char const *argv[])
 {
-	int arr[] = {1,3,7,9,22,111,344,599};
-	int data = 22;
-	int len = sizeof(arr) / 4;
-	printf("%d\n", BinarySearch(arr, len, data));
+	int nums[] = {1,2,2,2,3};
+	int len = sizeof(nums) / 4;
+	printf("%d\n", BinarySearch(nums, len, 2));
+	printf("%d\n", left_bound(nums, len, 2));
+	printf("%d\n", right_bound(nums, len, 2));	
 	return 0;
 }
